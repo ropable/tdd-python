dbe3e39b2c0b42df98caf5053fa79beebf5ebdea
Ashley Felton <ashley@ropable.com>
2014-01-07 14:23:38 +0800

    Created MVP functional test.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# Open the application home page.
+browser.get('http://localhost:8000')
+# Check that the page title is correct.
+assert 'To-Do' in browser.title
+# Quit the browser.
+browser.quit()
