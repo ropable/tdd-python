3369312d9f2c99864138d79bab243253276cf46e
Ashley Felton <ashley@ropable.com>
2014-01-07 14:55:57 +0800

    First unit test and URL mapping to dummy view.

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -3,10 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'superlists.views.home', name='home'),
+urlpatterns = patterns(
+    '',
+    url(r'^$', 'lists.views.home_page', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
