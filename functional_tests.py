13916c8f6baa63184d9a128a975c1c007688003f
Ashley Felton <ashley@ropable.com>
2014-01-07 14:33:47 +0800

    Functional tests now uses unittest, basic test runner class added.

--- functional_tests.py
+++ functional_tests.py
@@ -1,10 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
 
-# Open the application home page.
-browser.get('http://localhost:8000')
-# Check that the page title is correct.
-assert 'To-Do' in browser.title
-# Quit the browser.
-browser.quit()
+class newVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_open_homepage(self):
+        # Open the application home page.
+        self.browser.get('http://localhost:8000')
+
+        # Check that the page title is correct.
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish this test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
