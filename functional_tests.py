383ba58beb860264c15533905abcc8f8f2ae99ed
Ashley Felton <ashley@ropable.com>
2014-01-09 08:48:19 +0800

    Functional test checks we can input a to-do item (failing).

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -18,6 +19,21 @@ class newVisitorTest(unittest.TestCase):
         # Check that the page title is correct.
         self.assertIn('To-Do', self.browser.title)
 
+        # Check that any input field is present, with placeholder text.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # Input a to-do item.
+        inputbox.send_keys('Buy peacock feathers')
+
+        # After hitting ENTER, the page updates and now lists
+        # "1. Buy peacock feathers" as an item in a table.
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1.Buy peacock feathers' for row in rows))
+
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
