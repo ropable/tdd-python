0e96c706bf1f0e3937f50b8d18ecd1db8988928b
Ashley Felton <ashley@ropable.com>
2014-01-09 13:26:26 +0800

    Functional test passes (cheating)

--- functional_tests.py
+++ functional_tests.py
@@ -23,18 +23,20 @@ class newVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
-        # Input a to-do item.
+        # Input a to-do item and press ENTER to submit the form.
         inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Add a short pause after submitting before looking for elements,
+        # in order to view the CSRF error before the browser closes.
+        #import time
+        #time.sleep(2)
 
         # After hitting ENTER, the page updates and now lists
         # "1. Buy peacock feathers" as an item in a table.
-        inputbox.send_keys(Keys.ENTER)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1.Buy peacock feathers' for row in rows),
-            'New to-do item did not appear in table'
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         self.fail('Finish the test!')
 
--- lists/tests.py
+++ lists/tests.py
@@ -13,11 +13,27 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
+
         response = home_page(request)
+
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.strip().endswith(b'</html>'))
-        expected_html = render_to_string('home.html')
         # Use decode() to convert the response.content bytes into a unicode
         # string, so we can compare strings with strings instead of bytes.
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_post_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        #self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
         self.assertEqual(response.content.decode(), expected_html)
