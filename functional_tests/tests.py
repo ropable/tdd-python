fed32707a0b52ef35041224530847fe90cf94ab2
Ashley Felton <ashley@ropable.com>
2014-01-13 14:07:43 +0800

    Use Bootstrap to improve layout and styling.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -70,8 +70,6 @@ class newVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
-        self.fail('Finish the test!')
-
     def test_layout_and_styling(self):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
