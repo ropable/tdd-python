fe0e1d5df7d4190d03fd0afad2170deb698c9f56
Ashley Felton <ashley@ropable.com>
2014-01-13 09:43:43 +0800

    Update functional tests to reflect new desired behaviour for unique list URLs.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -28,25 +28,46 @@ class newVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
-        # Input a to-do item and press ENTER to submit the form.
+        # Edith inputs a to-do item and presses ENTER to submit the form.
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-
-        # Add a short pause after submitting before looking for elements,
-        # in order to view the CSRF error before the browser closes.
-        #import time
-        #time.sleep(2)
-
-        # After hitting ENTER, the page updates and now lists
+        # Edit is taken to a new URL, and the page lists:
         # "1. Buy peacock feathers" as an item in a table.
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        # Add a second item.
+        # Edith adds a second item.
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # A new user, Francis, visits the site. We use a new browser session
+        # for this user.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Visit the home page. There is no sign of Edit's list.
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a list new.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Francis gets redirected to his own unique URL.
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's list.
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
 
         self.fail('Finish the test!')
