56b47763f62883b7f1c7f6c2876a1e8da2414ea6
Ashley Felton <ashley@ropable.com>
2014-01-25 13:58:33 +0000

    Added logging config to settings.

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -88,3 +88,22 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '../django.log',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
