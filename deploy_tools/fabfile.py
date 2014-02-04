4e6ba7f57a92509e5b10b831309f90853cdf25ba
Ashley Felton <ashley@ropable.com>
2014-02-04 21:31:00 +0800

    Fix bug in fabfile, DB conf in settings.

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,8 +1,8 @@
 from fabric.contrib.files import append, exists, sed
-from fabric.api import env, local
+from fabric.api import env, local, run
 import random
 
-REPO_URL = 'https://ropable@bitbucket.org/ropable/tdd-python.git'
+REPO_URL = 'git@bitbucket.org:ropable/tdd-python.git'
 SITES_FOLDER = '/var/www'
 
 
@@ -21,24 +21,25 @@ def deploy():
 
 def _create_directory_structure_if_necessary(site_name):
     for subfolder in ('database', 'static', 'virtualenv', 'source', 'logs'):
-        local('mkdir -p {0}/{1}/{2}'.format(SITES_FOLDER, site_name, subfolder))
+        run('mkdir -p {0}/{1}/{2}'.format(SITES_FOLDER, site_name, subfolder))
 
 
 def _get_latest_source(source_folder):
     if exists(source_folder + '/.git'):
-        local('cd {0} && git fetch'.format(source_folder))
+        run('cd {0} && git fetch'.format(source_folder))
     else:
-        local('git clone {0} {1}'.format(REPO_URL, source_folder))
+        run('git clone {0} {1}'.format(REPO_URL, source_folder))
     current_commit = local('git log -n 1 --format=%H', capture=True)
-    local('cd {0} && git reset --hard {1}'.format(source_folder, current_commit))
+    run('cd {0} && git reset --hard {1}'.format(source_folder, current_commit))
 
 
 def _update_settings(source_folder, site_name):
     settings_path = source_folder + '/superlists/settings.py'
     sed(settings_path, 'DEBUG = True', 'DEBUG = False')
     sed(
+        settings_path,
         'ALLOWED_HOSTS = .+$',
-        'ALLOWED_HOSTS = ["{0}]'.format(site_name)
+        'ALLOWED_HOSTS = ["{0}"]'.format(site_name)
     )
     secret_key_file = source_folder + '/superlists/secret_key.py'
     if not exists(secret_key_file):  # 3
@@ -51,16 +52,16 @@ def _update_settings(source_folder, site_name):
 def _update_virtualenv(source_folder):
     virtualenv_folder = source_folder + '/../virtualenv'
     if not exists(virtualenv_folder + '/bin/pip'):
-        local('virtualenv --python=python3.3 {0}'.format(virtualenv_folder))
-    local('{0}/bin/pip install -r {1}/requirements.txt'.format(
+        run('virtualenv --python=python3.3 {0}'.format(virtualenv_folder))
+    run('{0}/bin/pip install -r {1}/requirements.txt'.format(
         virtualenv_folder, source_folder))
 
 
 def _update_static_files(source_folder):
-    local('cd {0} && ../virtualenv/bin/python3 manage.py collectstatic --noinput'.format(
+    run('cd {0} && ../virtualenv/bin/python3 manage.py collectstatic --noinput'.format(
         source_folder))
 
 
 def _update_database(source_folder):
-    local('cd {0} && ../virtualenv/bin/python manage.py syncdb --noinput'.format(
+    run('cd {0} && ../virtualenv/bin/python manage.py syncdb --noinput'.format(
         source_folder))
