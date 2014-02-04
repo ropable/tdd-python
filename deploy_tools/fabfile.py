97eadf56f4b44d91a931d7cc097cb8b244044138
Ashley Felton <ashley@ropable.com>
2014-02-04 21:06:30 +0800

    Fabfile pain.

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,5 +1,5 @@
 from fabric.contrib.files import append, exists, sed
-from fabric.api import env, local, run
+from fabric.api import env, local
 import random
 
 REPO_URL = 'https://ropable@bitbucket.org/ropable/tdd-python.git'
@@ -21,16 +21,16 @@ def deploy():
 
 def _create_directory_structure_if_necessary(site_name):
     for subfolder in ('database', 'static', 'virtualenv', 'source', 'logs'):
-        run('mkdir -p {0}/{1}/{2}'.format(SITES_FOLDER, site_name, subfolder))
+        local('mkdir -p {0}/{1}/{2}'.format(SITES_FOLDER, site_name, subfolder))
 
 
 def _get_latest_source(source_folder):
     if exists(source_folder + '/.git'):
-        run('cd {0} && git fetch'.format(source_folder))
+        local('cd {0} && git fetch'.format(source_folder))
     else:
-        run('git clone {0} {1}'.format(REPO_URL, source_folder))
+        local('git clone {0} {1}'.format(REPO_URL, source_folder))
     current_commit = local('git log -n 1 --format=%H', capture=True)
-    run('cd {0} && git reset --hard {1}'.format(source_folder, current_commit))
+    local('cd {0} && git reset --hard {1}'.format(source_folder, current_commit))
 
 
 def _update_settings(source_folder, site_name):
@@ -51,16 +51,16 @@ def _update_settings(source_folder, site_name):
 def _update_virtualenv(source_folder):
     virtualenv_folder = source_folder + '/../virtualenv'
     if not exists(virtualenv_folder + '/bin/pip'):
-        run('virtualenv --python=python3.3 {0}'.format(virtualenv_folder))
-    run('{0}/bin/pip install -r {1}/requirements.txt'.format(
+        local('virtualenv --python=python3.3 {0}'.format(virtualenv_folder))
+    local('{0}/bin/pip install -r {1}/requirements.txt'.format(
         virtualenv_folder, source_folder))
 
 
 def _update_static_files(source_folder):
-    run('cd {0} && ../virtualenv/bin/python3 manage.py collectstatic --noinput'.format(
+    local('cd {0} && ../virtualenv/bin/python3 manage.py collectstatic --noinput'.format(
         source_folder))
 
 
 def _update_database(source_folder):
-    run('cd {0} && ../virtualenv/bin/python manage.py syncdb --noinput'.format(
+    local('cd {0} && ../virtualenv/bin/python manage.py syncdb --noinput'.format(
         source_folder))
