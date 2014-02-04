16bda440db7710e045b72262d5c814fc547c232a
Ashley Felton <ashley@ropable.com>
2014-02-04 08:01:04 +0800

    Finished fabfile (untested).

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,4 +1,4 @@
-from fabric.contrib.files import append, exists, sed, run
+from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run
 import random
 
@@ -30,5 +30,34 @@ def _get_latest_source(source_folder):
     run('cd {0} && git reset --hard {1}'.format(source_folder, current_commit))
 
 
+def _update_settings(source_folder, site_name):
+    settings_path = source_folder + '/superlists/settings.py'
+    sed(settings_path, 'DEBUG = True', 'DEBUG = False')
+    sed(
+        'ALLOWED_HOSTS = .+$',
+        'ALLOWED_HOSTS = ["{0}]'.format(site_name)
+    )
+    secret_key_file = source_folder + '/superlists/secret_key.py'
+    if not exists(secret_key_file):  # 3
+        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+        key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
+        append(secret_key_file, "SECRET_KEY = '%s'" % (key,))
+    append(settings_path, '\nfrom .secret_key import SECRET_KEY')
+
+
+def _update_virtualenv(source_folder):
+    virtualenv_folder = source_folder + '/../virtualenv'
+    if not exists(virtualenv_folder + '/bin/pip'):
+        run('virtualenv --python=python3.3 {0}'.format(virtualenv_folder))
+    run('{0}/bin/pip install -r {1}/requirements.txt'.format(
+        virtualenv_folder, source_folder))
+
+
+def _update_static_files(source_folder):
+    run('cd {0} && ../virtualenv/bin/python3 manage.py collectstatic --noinput'.format(
+        source_folder))
+
+
 def _update_database(source_folder):
-    run('cd %s && ../virtualenv')
+    run('cd {0} && ../virtualenv/bin/python manage.py syncdb --noinput'.format(
+        source_folder))
