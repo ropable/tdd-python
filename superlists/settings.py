d92962535eebc0d811fbe40c04b82c65cb89df0d
Ashley Felton <ashley@ropable.com>
2014-02-15 21:05:38 +0800

    Bunch of housekeeping around static files. Started accounts app.

--- superlists/settings.py
+++ superlists/settings.py
@@ -83,6 +83,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'superlists', 'static'),
+)
 
 LOGGING = {
     'version': 1,
